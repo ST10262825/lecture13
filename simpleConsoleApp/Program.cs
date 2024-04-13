4c17526c3074cdbf554758b0e4eb8581d788b67f
ST10262825 <st10262825@vcconnect.edu.za>
2024-04-13 22:55:38 +0200

    first commit

---
+++ simpleConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your name: ");// prompt the user for  input
+string userInput = Console.ReadLine();//Read user input from the console 
+Console.WriteLine("Your name is: " + userInput);// Display back what the user had entered when prompted
